15bb19a5be8d0a981e4ff635ae997783ba971987
Martin Valba <kailing2@live.com>
2021-05-23 15:02:24 -0400

    Alot of changes

--- manual/build.sh
+++ manual/build.sh
@@ -6,20 +6,21 @@ echo "Building new packages"
 dpkg-deb --build apktool
 dpkg-deb --build metasploit-framework
 dpkg-deb --build neofetch
-
-dpkg-deb --build cus-glib
 dpkg-deb --build ngrok
 dpkg-deb --build msfpc
-dpkg-deb --build nethunter-support
 dpkg-deb --build android-pin-bruteforce
 dpkg-deb --build john
 dpkg-deb --build psf
+dpkg-deb --build gradle
+dpkg-deb --build android-sdk-build-tools
+dpkg-deb --build ant
 
 echo "Untarring jdk-11 as github dosent like 100mb+ files"
 tar -xf openjdk-jdk11.tar.gz
 dpkg-deb --build openjdk-jdk11
 rm -rf openjdk-jdk11
 
+mkdir -p ../debs
 mv -f *.deb ../debs
-echo "Done"
+echo "All packages are moved to ../debs"
 
